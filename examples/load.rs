c7a6af6fe5606a8ed642c3e35f618c0d77db79bd
TeaSpillStudios <teaspillstudios@proton.me>
2023-04-02 03:35:47 +0100

    Load the markdown test file as an absolute path

--- examples/load.rs
+++ examples/load.rs
@@ -2,5 +2,9 @@ use marse::*;
 use std::path::Path;
 
 fn main() {
-    println!("{}", file_to_html(Path::new("./test.md")).unwrap());
+    println!(
+        "{}",
+        file_to_html(&Path::new(env!("CARGO_MANIFEST_DIR")).join(Path::new("examples/test.md")))
+            .unwrap()
+    );
 }
