b53f7971f1843afcce535ea64d9bd53dd7dc0b63
TeaSpillStudios <teaspillstudios@proton.me>
2023-04-02 03:36:24 +0100

    Setup logging in the load example

--- examples/load.rs
+++ examples/load.rs
@@ -2,6 +2,8 @@ use marse::*;
 use std::path::Path;
 
 fn main() {
+    pretty_env_logger::init_custom_env("MARSE_LOG");
+
     println!(
         "{}",
         file_to_html(&Path::new(env!("CARGO_MANIFEST_DIR")).join(Path::new("examples/test.md")))
