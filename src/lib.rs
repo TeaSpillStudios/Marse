bdbebca84cfd54a5930a9dbde420f8e361a7d36d
TeaSpillStudios <teaspillstudios@proton.me>
2023-04-02 04:15:15 +0100

    Match headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use log::{debug, error};
+use regex::Regex;
 use std::fs::File;
 use std::io::{BufReader, Read};
 use std::path::Path;
@@ -11,6 +12,13 @@ pub enum MarseError {
 pub fn file_to_html(path: &Path) -> Result<String, MarseError> {
     debug!("Loading {path:#?}");
 
+    let h1 = Regex::new("^#(.[a-zA-Z0-9])").unwrap();
+    let h2 = Regex::new("^##(.[a-zA-Z0-9])").unwrap();
+    let h3 = Regex::new("^###(.[a-zA-Z0-9])").unwrap();
+    let h4 = Regex::new("^####(.[a-zA-Z0-9])").unwrap();
+    let h5 = Regex::new("^#####(.[a-zA-Z0-9])").unwrap();
+    let h6 = Regex::new("^######(.[a-zA-Z0-9])").unwrap();
+
     let file = match File::open(path) {
         Ok(v) => v,
         Err(e) => {
@@ -21,6 +29,7 @@ pub fn file_to_html(path: &Path) -> Result<String, MarseError> {
 
     let mut reader = BufReader::new(file);
     let mut file_contents = String::new();
+    let mut html: Vec<String> = Vec::new();
 
     match reader.read_to_string(&mut file_contents) {
         Ok(v) => v,
@@ -30,5 +39,26 @@ pub fn file_to_html(path: &Path) -> Result<String, MarseError> {
         }
     };
 
-    Ok(file_contents)
+    for line in file_contents.lines() {
+        if h1.is_match(line) {
+            html.push(format!("<h1>{line}</h1>"));
+        }
+        if h2.is_match(line) {
+            html.push(format!("<h2>{line}</h2>"));
+        }
+        if h3.is_match(line) {
+            html.push(format!("<h3>{line}</h3>"));
+        }
+        if h4.is_match(line) {
+            html.push(format!("<h4>{line}</h4>"));
+        }
+        if h5.is_match(line) {
+            html.push(format!("<h5>{line}</h5>"));
+        }
+        if h6.is_match(line) {
+            html.push(format!("<h6>{line}</h6>"));
+        }
+    }
+
+    Ok(html.iter().map(|s| format!("{s}\n")).collect::<String>())
 }
